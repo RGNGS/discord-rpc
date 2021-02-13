e2372e053bd528aa73926b08c4bd8c43ab6e569b
Oskar <saturdaysheroes.business@gmail.com>
2021-02-13 18:56:50 +0100

    use player nick in rpc

--- lua/cl_discord.lua
+++ lua/cl_discord.lua
@@ -7,13 +7,12 @@ local strAppID = "810178281956573244"
 local intStart = intStart || -1
 
 local function DiscordUpdate()
-    DiscordRPCTime("CityRP", "Oscar Sabato", "default", game.GetMap(), player.GetCount(), (game.MaxPlayers() || "48"), intStart)
+    DiscordRPCTime("CityRP", (LocalPlayer():Nick() || "Łączenie..."), "default", game.GetMap(), player.GetCount(), (game.MaxPlayers() || "48"), intStart)
 end
 
 hook.Add("Initialize", "RGngs.DiscordRPC", function()
     intStart = os.time()
     DiscordRPCInitialize(strAppID)
-    DiscordUpdate()
 
     timer.Create("RGngs.DiscordRPC", intCheck, 0, DiscordUpdate)
 end)
